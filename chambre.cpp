8a2f355c4465cf9513f874ad3ff2c723443b2b87
NathanDSZ <75113748+NathanDSZ@users.noreply.github.com>
2020-11-27 10:43:16 +0100

    Create chambre.cpp

---
+++ chambre.cpp
@@ -0,0 +1,27 @@
+#include "chambre.h"
+
+namespace genre {
+	
+	Chambre::Chambre(int id, genre type, int price) : _id(id), _type(type), _price(price) {};
+	
+	int Chambre::id() const {
+		return _id;
+	};
+
+	genre Chambre::type() const {
+		return _type;
+	};
+
+	int Chambre::price() const {
+		return _price;
+	};
+
+	void Chambre::setId(int id) {
+		_id = id;
+	};
+
+	void setType(genre type) { _type = type };
+
+	void setPrice(int price) { _price = price };
+
+};
